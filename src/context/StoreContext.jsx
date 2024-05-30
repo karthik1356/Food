4b3eb19204323fe776ef5c265828e59cf5431a84
siddhu_from_sklm <codewithsiddhu@gmail.com>
2024-05-30 12:08:03 +0530

    Create tailwind Properties

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,9 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url='http://localhost:4000';
+  const url='https://foodbackend-ireb.onrender.com';
+
+  
   const [token,setToken]=useState('');
   const [food_list,setFood_list]=useState([]);
 
